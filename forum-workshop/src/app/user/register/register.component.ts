e253603f19cba13c46ffe02112fa5d750b5d0ecc
Ivaylo Ganev <ivayloganev@gmail.com>
2023-07-16 21:43:01 +0300

    Validation of registercomponent

--- forum-workshop/src/app/user/register/register.component.ts
+++ forum-workshop/src/app/user/register/register.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { appEmailValidator } from 'src/app/shared/validators/app-email-validaror';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/validators/constants';
+import { matchPasswordsValidator } from 'src/app/shared/validators/match-password-validator';
 
 @Component({
   selector: 'app-register',
@@ -8,11 +12,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)]],
+    tel: [""],
+    passGroup: this.fb.group({
+      password: ['', [Validators.required, Validators.minLength(5)]],
+      rePassword: ['', [Validators.required]]
+    },
+    {
+      validators: [matchPasswordsValidator('password', 'rePassword')]
+    }
+    )
+  })
 
-  constructor(private userService: UserService, private router: Router) {}
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {}
 
-  register(username: string, email: string, phone: string, password: string, rePass: string): void {
-    this.userService.register();
-    this.router.navigate(['/']);
+  register(): void {
+    if(this.form.invalid) {
+      return;
+    }
+    
+
+    // this.userService.register();
+    // this.router.navigate(['/']);
   }
 }
